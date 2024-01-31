782600a5cfb370b1e514a02c9ccaa416800a85d5
Vern Townsend <107884893+Vtown121@users.noreply.github.com>
2024-01-31 14:09:21 -0700

    chap 6

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,8 +8,7 @@ require "rulers/file_model"
 
 module Rulers
   class Application
-    def call(env)
-
+    def call(env) 
       if env['PATH_INFO'] == '/favicon.ico'
         return [404,
           {'Content-Type' => 'text/html'}, []]
@@ -18,7 +17,12 @@ module Rulers
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      r = controller.get_response
+      if r
+        [r.status, r.headers, [r.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      end
     end
   end
 end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -4,6 +4,7 @@ require "rulers/file_model"
 
 module Rulers
   class Controller
+  include Rulers::Model
     def initialize(env)
       @env = env
     end
@@ -31,6 +32,28 @@ module Rulers
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(:env => env)
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+    
+    def get_response  # Only for Rulers
+      @response
+    end
+    
+    def render_response(*args)
+      response(render(*args))
     end
   end
 end
